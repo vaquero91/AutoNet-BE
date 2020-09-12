c5004cc46efc175adda2857c52fb3efb8d818bad
vaquero91 <55334641+vaquero91@users.noreply.github.com>
2020-09-12 18:58:15 -0500

    First Commit
    primer build

---
+++ autoNet-BE/src/main/java/com/myTwitterAPI/autoNetBE/AutoNetBeApplication.java
@@ -0,0 +1,13 @@
+package com.myTwitterAPI.autoNetBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoNetBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoNetBeApplication.class, args);
+	}
+
+}
---
+++ autoNet-BE/src/test/java/com/myTwitterAPI/autoNetBE/AutoNetBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myTwitterAPI.autoNetBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoNetBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
