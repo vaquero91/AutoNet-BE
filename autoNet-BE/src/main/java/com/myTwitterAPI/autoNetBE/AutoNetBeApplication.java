f9740373225bfb37647c77f61297349d14561568
Sergio Hurtado <sergio.hurtado@udem.edu>
2020-09-12 19:30:12 -0500

    Autentification JWT added

--- autoNet-BE/src/main/java/com/myTwitterAPI/autoNetBE/AutoNetBeApplication.java
+++ autoNet-BE/src/main/java/com/myTwitterAPI/autoNetBE/AutoNetBeApplication.java
@@ -1,7 +1,23 @@
 package com.myTwitterAPI.autoNetBE;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.Collections;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AutoNetBeApplication {
@@ -9,5 +25,36 @@ public class AutoNetBeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AutoNetBeApplication.class, args);
 	}
+           @Bean   
+        public FilterRegistrationBean<CorsFilter> simpleCorsFilter() {
+            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+            CorsConfiguration config = new CorsConfiguration();
+            config.setAllowCredentials(true);
+            config.setAllowedOrigins(Collections.singletonList("*"));
+            config.setAllowedMethods(Collections.singletonList("*"));
+            config.setAllowedHeaders(Collections.singletonList("*"));
+            source.registerCorsConfiguration("/**", config);
+            FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+            bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+            
+            return bean;
+        }
+     
+        	
+        
+        
+        @EnableWebSecurity
+        @Configuration
+        class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().disable()
+				.addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+				.authorizeRequests()
+                                .antMatchers(HttpMethod.GET, "/").permitAll()
+				.anyRequest().authenticated();
+		}
+        }
 
 }
