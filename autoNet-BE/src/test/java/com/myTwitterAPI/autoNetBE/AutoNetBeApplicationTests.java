27a9fad5751d3f822d523806bcfaefa92e8a0b58
Juan Hurtado <55334641+vaquero91@users.noreply.github.com>
2020-10-20 23:14:47 -0500

    testing added and travis file

--- autoNet-BE/src/test/java/com/myTwitterAPI/autoNetBE/AutoNetBeApplicationTests.java
+++ autoNet-BE/src/test/java/com/myTwitterAPI/autoNetBE/AutoNetBeApplicationTests.java
@@ -1,13 +1,28 @@
 package com.myTwitterAPI.autoNetBE;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
-@SpringBootTest
+
+@WebMvcTest(testRest.class)
+@RunWith(SpringRunner.class)
 class AutoNetBeApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
+        
+        @Autowired
+        private MockMvc mockMvc;
+        
+        @Test
+        public void test() throws Exception {
+            mockMvc.perform(get("/")).andExpect(status().isOk());
+        }
+	
+                
+    
 
 }
