e80b22acbac7e11a998c671c2e5ea5bd65c5c86b
Juan Hurtado <55334641+vaquero91@users.noreply.github.com>
2020-11-10 20:49:41 -0600

    twitterDriver archivo creado

---
+++ autoNet-BE/src/main/java/com/myTwitterAPI/autoNetBE/TwitterDriver.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.myTwitterAPI.autoNetBE;
+
+import java.io.IOException;
+import twitter4j.Paging;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.Query;
+import java.util.ArrayList;
+import java.util.List;
+import twitter4j.QueryResult;
+/**
+ *
+ * @author juanh
+ */
+public class TwitterDriver {
+    
+}
